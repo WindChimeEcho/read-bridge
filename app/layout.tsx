f76e6b4029f51d71fdaf8d43b32613b0344e36d2
Echo <chroom@foxmail.com>
2025-08-26 10:24:21 +0800

    style:Add overflow-hidden to prevent certain plugins from affecting the overall scrollbar.

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning className="h-full">
+    <html lang="en" suppressHydrationWarning className="h-full overflow-hidden">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=yes" />
       </head>
